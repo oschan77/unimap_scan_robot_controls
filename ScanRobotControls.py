f8ff8b9199de2efce1f68f6f53c04751bf122679
oschan77 <oschan77@gmail.com>
2024-06-27 10:28:03 +0800

    Add Non-blocking

--- ScanRobotControls.py
+++ ScanRobotControls.py
@@ -2,12 +2,14 @@ import glob
 import os
 import signal
 import subprocess
+import threading
 
 
 class ScanRobotControls:
     def __init__(self):
         self.rosbag_dir_path = "/home/u/rosbag"
         self.proc_record_pid = None
+        self.record_thread = None
         self.password = "u"
 
     def get_latest_created_folder(self):
@@ -34,22 +36,29 @@ class ScanRobotControls:
 
         return proc.returncode, stdout, stderr, proc.pid
 
-    def record(self):
-        cmd_record = "docker exec rosbag bash -c 'ros2 bag record /ouster/points /ouster/imu /ouster/scan /odom'"
+    def record_process(self):
+        cmd_record = "docker exec rosbag bash -c 'source /opt/ros/humble/setup.bash ; ros2 bag record /ouster/points /ouster/imu /ouster/scan /odom'"
         returncode, stdout, stderr, pid = self.run_with_sudo(cmd_record)
         self.proc_record_pid = pid
 
         return returncode, stdout, stderr
 
+    def record(self):
+        self.record_thread = threading.Thread(target=self.record_process)
+        self.record_thread.start()
+
     def stop(self):
         if self.proc_record_pid is not None:
             os.killpg(os.getpgid(self.proc_record_pid), signal.SIGTERM)
             self.proc_record_pid = None
+            if self.record_thread is not None:
+                self.record_thread.join()
+                self.record_thread = None
 
     def convert(self):
         ros2bag_folder = self.get_latest_created_folder()
         if ros2bag_folder:
-            cmd_convert = f"docker exec rosbag bash -c 'rosbags-convert --src {ros2bag_folder} --dst {ros2bag_folder}.bag'"
+            cmd_convert = f"docker exec rosbag bash -c 'source /opt/ros/humble/setup.bash ; rosbags-convert --src {ros2bag_folder} --dst {ros2bag_folder}.bag'"
 
             return self.run_with_sudo(cmd_convert)
 
