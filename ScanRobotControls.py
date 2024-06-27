c2a230f545835e401dac4b6ddcb2f851a4191ae4
oschan77 <oschan77@gmail.com>
2024-06-27 11:41:44 +0800

    Remove sudo

--- ScanRobotControls.py
+++ ScanRobotControls.py
@@ -81,7 +81,6 @@ class ScanRobotControls:
     def __init__(self):
         self.rosbag_dir_path = "/home/u/rosbag"
         self.proc_record = None
-        self.password = "u"
 
     def get_latest_created_folder(self):
         all_folders = [
@@ -92,31 +91,29 @@ class ScanRobotControls:
 
         return max(all_folders, key=lambda x: os.path.getctime(x), default=None)
 
-    def run_with_sudo(self, cmd: str):
+    def run_command(self, cmd: str):
         proc = subprocess.Popen(
-            f"echo {self.password} | sudo -S {cmd}",
+            cmd,
             shell=True,
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
-            preexec_fn=os.setsid,
+            preexec_fn=os.setsid,  # Start the process in a new session
         )
         stdout, stderr = proc.communicate()
         return proc.returncode, stdout, stderr, proc.pid
 
     def record(self):
-        cmd_record = "docker exec rosbag bash -c 'source /opt/ros/humble/setup.bash ; ros2 bag record /ouster/points /ouster/imu /ouster/scan /odom'"
+        cmd_record = "docker exec rosbag bash -c 'source /opt/ros/humble/setup.bash; ros2 bag record /ouster/points /ouster/imu /ouster/scan /odom'"
         self.proc_record = subprocess.Popen(
-            f"echo {self.password} | sudo -S {cmd_record}",
+            cmd_record,
             shell=True,
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
-            preexec_fn=os.setsid,
+            preexec_fn=os.setsid,  # Start the process in a new session
         )
-        time.sleep(1)  # Give some time for the process to start and get its PID
-        self.proc_record.stdin.write(self.password.encode())
-        self.proc_record.stdin.flush()
+        time.sleep(1)  # Give some time for the process to start
 
     def stop(self):
         if self.proc_record is not None:
@@ -128,10 +125,10 @@ class ScanRobotControls:
     def convert(self):
         ros2bag_folder = self.get_latest_created_folder()
         if ros2bag_folder:
-            cmd_convert = f"docker exec rosbag bash -c 'source /opt/ros/humble/setup.bash ; rosbags-convert --src {ros2bag_folder} --dst {ros2bag_folder}.bag'"
-            return self.run_with_sudo(cmd_convert)
+            cmd_convert = f"docker exec rosbag bash -c 'source /opt/ros/humble/setup.bash; rosbags-convert --src {ros2bag_folder} --dst {ros2bag_folder}.bag'"
+            return self.run_command(cmd_convert)
 
         return 1, b"", b"Folder not found"
 
     def test(self):
-        return self.run_with_sudo("ls")
+        return self.run_command("ls")
